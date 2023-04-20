636e3ffb2aa7b6e5fd4dad4e2ec582e0d419194f
DanuchaP <danucha.phol@gmail.com>
2023-04-20 16:24:06 +0700

    Edit: Search box / Add: Icon droupdown

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,59 +1,67 @@
 import { NavLink, Link } from "react-router-dom";
-
+import "../../assets/navbar.css";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar bg-body-tertiary">
+      <nav className="navbar bg-body-tertiary outter-nav">
         <div className="container">
           <NavLink className="navbar-brand" to="#">
             <img
-              src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+              src="https://placehold.co/600x400/orange/white"
               alt="NestFit"
               width={30}
               height={24}
             />
           </NavLink>
+
           <form className="d-flex" role="search">
             <input
-              className="form-control me-2"
+              className="search-nav"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
-              Search
+            <button
+              className="image-search-button"
+            >
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-search-strong-256.png"
+                alt=""
+                width={30}
+              />
             </button>
           </form>
-
-          <img
-            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
-            alt="NestFit"
-            aria-expanded="false"
-            width={30}
-            height={24}
-          >
-            <NavLink className="navbar-brand" to="#">
-              <li className="nav-item dropdown">
-                <ul className="dropdown-menu dropdown-menu-dark">
-                  <li>
-                    <Link className="dropdown-item" to="#">
-                      Action
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="#">
-                      Another action
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="#">
-                      Something else here
-                    </Link>
-                  </li>
-                </ul>
+          
+          <div className="dropdown container-dropdown">
+            <button
+              className="image-button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png"
+                alt=""
+                width={30}
+              />
+            </button>
+            <ul className="dropdown-menu drop-list">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
               </li>
-            </NavLink>
-          </img>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
