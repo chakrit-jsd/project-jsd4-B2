00f2be5792c9e957ead8b64cbc0ba23958e6dbab
chakrit-jsd <chakritabv@gmail.com>
2023-04-22 13:17:37 +0700

    Edit: error message validate Register

--- src/utils/validators/validateRegister.js
+++ src/utils/validators/validateRegister.js
@@ -4,17 +4,17 @@ import { provincesThailand } from '../../assets/data/provinceList'
 const schema = yup.object().shape({
   email: yup
     .string()
-    .required('กรุณากรอก email')
-    .min(6, 'email สั้นเกินไป')
-    .max(50, 'email ยาวเกินไป')
-    .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, 'not')
+    .required('Please input your Email')
+    .min(6, 'Email must 6 - 50 character')
+    .max(50, 'Email must 6 - 50 character')
+    .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, 'Invalid Email Pattern')
     .trim(),
 
   password: yup
     .string()
-    .required('password')
-    .min(8, 'short')
-    .max(30, 'long')
+    .required('Password is required')
+    .min(8, 'Password must 8 - 30 character')
+    .max(30, 'Password must 8 - 30 character')
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, 'Password must contain at least 1 (A-Z), (a-z), and (0-9)')
     .trim(),
 
