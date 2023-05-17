bd34c978dae366127c1fce3c1d179608bb6dd01b
DanuchaP <danucha.phol@gmail.com>
2023-05-17 17:22:35 +0700

    frame layout

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,9 +1,62 @@
+import "../../assets/styles/dashboard.css";
+
 const Dashboard = () => {
   return (
     <aside className="container-dashboard col-xl-2 col-lg-2 col-md-1">
-      Dashboard
-    </aside>
-  )
-}
+      <div>Circle Graph</div>
 
-export default Dashboard
+      <main className="activity-detail-box">
+        <div className="activity-detail" id="act-1-detail">
+          <span className="act-name">
+            Yoga
+          </span>
+          <span className="act-time">
+            10min
+          </span>
+        </div>
+        <div className="activity-detail" id="act-2-detail">
+          <span className="act-name">
+            HIIT
+          </span>
+          <span className="act-time">
+            10min
+          </span>
+        </div>
+        <div className="activity-detail" id="act-3-detail">
+          <span className="act-name">
+            Weight Training
+          </span>
+          <span className="act-time">
+            10min
+          </span>
+        </div>
+        <div className="activity-detail" id="act-4-detail">
+          <span className="act-name">
+            Strength Training
+          </span>
+          <span className="act-time">
+            10min
+          </span>
+        </div>
+        <div className="activity-detail" id="act-5-detail">
+          <span className="act-name">
+            Pilates
+          </span>
+          <span className="act-time">
+            10min
+          </span>
+        </div>
+      </main>
+
+      <footer className="achievement">
+        <span className="bagde-1">B</span>
+        <span className="bagde-1">B</span>
+        <span className="bagde-1">B</span>
+        <span className="bagde-1">B</span>
+        <span className="bagde-1">B</span>
+      </footer>
+    </aside>
+  );
+};
+
+export default Dashboard;
