39283c9c5c2a7d58876ef56c6b66d510a060f750
chakrit-jsd <chakritabv@gmail.com>
2023-05-17 16:27:43 +0700

    Add: vercel.json

--- src/services/API/_confingAxios.js
+++ src/services/API/_confingAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL : 'http://localhost:3002',
+  baseURL : 'http://139.59.103.113',
   withCredentials: true,
   headers : {
       'Content-Type': 'application/json',
