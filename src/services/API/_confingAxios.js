d8c950ba5eecae9fbc4439573509815589f9ca7b
chakrit-jsd <chakritabv@gmail.com>
2023-05-17 17:26:02 +0700

    Edit: axios

--- src/services/API/_confingAxios.js
+++ src/services/API/_confingAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL : 'http://139.59.103.113',
+  baseURL : 'http://139.59.103.113:3002',
   withCredentials: true,
   headers : {
       'Content-Type': 'application/json',
